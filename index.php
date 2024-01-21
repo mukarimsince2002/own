435f1237d58d219a3cc1e9d48f0ae4a4f2e492b6
mukarimsince2002 <mohammadhmukarim@gmail.com>
2024-01-21 21:31:54 +0530

    add css and js files

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>e-commerce</title>
+    <link href="fa/css/svg-with-js.css">
+    <link rel="stylesheet" href="css/jquery.bxslider.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">    
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">    
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div class="wrapper">
+    <div class="navigation-bar">
+        <div class="container">
+            <!--insert a logo-->
+            <div class="logo">
+                <img src="images/logo.png" alt="cart" >
+            </div>
+            <!-- search box-->
+            <div class="search-area">
+            <form action="" method="post">
+                <input type="text" name="search_box" class="search_box" placeholder="Search all items...">
+                <button class="search_btn btn btn-success"><i class="fas fa-search"></i></button>
+            </form>
+            </div>
+            <!-- user menu-->
+            <div class="user-menu">
+                <ul>
+                    <li><a href="#"><i class="fas fa-user "></i>My Account</a></li>
+                    <li><a href="#"><i class="fas fa-shopping-cart"></i>cart</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.bxslider.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="fa/js/all.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
