8be008d0bfcd7d2590ac211544e10bfa644c5f55
mukarimsince2002 <mohammadhmukarim@gmail.com>
2024-01-22 22:54:30 +0530

    add css and js files

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>e-commerce</title>
     <link href="fa/css/svg-with-js.css">
-    <link rel="stylesheet" href="css/jquery.bxslider.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">    
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">    
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">   
+        <link rel="stylesheet" href="css/jquery.bxslider.min.css"> 
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
@@ -29,7 +29,19 @@
             <!-- user menu-->
             <div class="user-menu">
                 <ul>
-                    <li><a href="#"><i class="fas fa-user "></i>My Account</a></li>
+                    <li><a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user "></i>My Account</a>
+                    <!--dropdown box-->
+                    <div class="dropdown-menu dropdown-menu-right bg-dark" style="width: 250px;">
+                        <a href="#"><button type="button" class="dropdown-item"><i class="fas fa-user "></i>&nbsp; your account</button></a>
+                        <a href="#"><button type="button" class="dropdown-item"><i class="fas fa-cube "></i> &nbsp; your order</button></a>
+                        <a href="#"><button type="button" class="dropdown-item"><i class="fas fa-heart "></i> &nbsp; wishist</button></a>
+                        <div class="dropdown-divider">
+                            <p class="text-center text-white" style="height:15px;line: height 20px;"><small>if you are a new user</small></p>
+                            <a href="#"><button type="button" class="dropdown-item text-center"><i class="fas fa-user "></i> &nbsp; Register </button></a>
+                            <a href="#"><button type="button" class="dropdown-item text-center bg-danger"><i class="fas fa-user "></i> &nbsp; Login </button></a>
+                        </div>
+                    </div>
+                </li>
                     <li><a href="#"><i class="fas fa-shopping-cart"></i>cart</a></li>
                 </ul>
             </div>
@@ -38,10 +50,9 @@
 </div>
 
     <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
     <script src="js/jquery.bxslider.min.js"></script>
     <script src="js/owl.carousel.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
     <script src="fa/js/all.js"></script>
     <script src="js/main.js"></script>
 </body>
